488f3f002325261e330df6f6925d250b1c59ca71
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-02-15 11:53:53 +0200

    autoload ir router

--- core/Router.php
+++ core/Router.php
@@ -10,9 +10,39 @@ namespace app\core;
  */
 class Router
 {
+    /**
+     * This holds all routes
+     * 
+     * routes[
+     * ['get' => [
+     * ['/' => function return,],
+     * ['/about' => function return,],
+     * ],
+     * ['post' => [
+     * ['/' => function return,],
+     * ['/about' => function return,],
+     * ],
+     * 
+     * 
+     */
+    protected array $routes = [];
+
     public function __construct()
     {
         echo "This is router constructor <br>";
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        echo "<pre>";
+        var_dump($_SERVER);
+        echo "</pre>";
+        // exit;
     }
 
     
