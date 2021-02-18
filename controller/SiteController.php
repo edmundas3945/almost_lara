58fdb29994326e4964984f8fa35f4985c75f2b05
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-02-18 10:55:55 +0200

    blah blah

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -2,10 +2,28 @@
 
 namespace app\controller;
 
+use app\core\Application;
+
 class SiteController
 {
+    /**
+     * This is where we handle post contact form     
+     * 
+     * @return string
+     */
     public static function handleContact()
     {
         return "handling form contact from Site controller";
+    }
+    /**
+     * This serves the contact view
+     *
+     * @return string
+     */
+    public static function contact()
+    {
+        //lets render view
+        // return "This should be a form";
+        return Application::$app->router->renderView('contact');
     }
 }
