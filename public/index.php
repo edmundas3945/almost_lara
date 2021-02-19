8b4e59d56bbc775cde12a16ffb529a59e929380d
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-02-19 11:15:07 +0200

    first we add routes and controllers reference

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 // phpinfo();exit;
+
+use app\core\AuthController;
+
 require_once '../vendor/autoload.php';
 
 
@@ -13,5 +16,13 @@ $app->router->get('/about', [\app\controller\SiteController::class, 'about']);
 $app->router->get('/contact', [\app\controller\SiteController::class, 'contact']);
 
 $app->router->post('/contact', [\app\controller\SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+
+$app->router->post('/login', [AuthController::class, 'login']);
+
+$app->router->get('/register', [AuthController::class, 'register']);
+
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
